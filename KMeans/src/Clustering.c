3ac27a8279b193e29061a9c7f24ed0a85431e08d
klivs <kostas.livs@gmail.com>
2022-05-16 19:56:57 +0300

    Code clean up

--- KMeans/src/Clustering.c
+++ KMeans/src/Clustering.c
@@ -7,6 +7,7 @@
 
 #include <math.h>
 #include <stdlib.h>
+#include <stdio.h>
 
 #include "Clustering.h"
 
@@ -39,10 +40,10 @@ void KMeans( double patterns[][LENGTH], double c[CENTERS][LENGTH], int numP ) {
               d[i][j] = distEucl( patterns[i], c[j] );
             a[i] = argMin( d[i] ) ;
         }
-        //
-        double y[CENTERS][LENGTH];
-        double z[CENTERS][LENGTH];
-        initialTmpArray(y,z);
+
+        //Fill two arrays with 0.0, they used for recalculated the means
+        double y[CENTERS][LENGTH] = {{0.0}};
+        double z[CENTERS][LENGTH] = {{0.0}};
         //update step
         for ( int i = 0; i < numP; i++ ) {
             int index = a[i];
@@ -70,6 +71,17 @@ void KMeans( double patterns[][LENGTH], double c[CENTERS][LENGTH], int numP ) {
     return ;
 }
 
+// Calculates the Eucledean distance between a pattern and a centroid.
+double distEucl( double x[LENGTH], double c[LENGTH] ) {
+
+	double distance = 0;
+
+    for ( int i = 0; i < LENGTH; i++ )
+        distance += pow( (x[i] - c[i]), 2.0 );
+
+    return ( pow(distance, 0.5) );
+}
+
 // Argminimum
 int argMin( double d[CENTERS] ) {
 
@@ -84,17 +96,6 @@ int argMin( double d[CENTERS] ) {
     }
 
     return index;
-}
-
-// Calculates the Eucledean distance between a pattern and a centroid.
-double distEucl( double x[LENGTH], double c[LENGTH] ) {
-
-	double distance = 0;
-
-    for ( int i = 0; i < LENGTH; i++ )
-        distance += pow( (x[i] - c[i]), 2.0 );
-
-    return ( pow(distance, 0.5) );
 }
 
 // Fill two arrays with 0.0, they used for recalculated the means.
@@ -122,4 +123,100 @@ int classifyPattern( double pattern[LENGTH], double c[CENTERS][LENGTH] ) {
 	class = argMin( distances ) + 1;
 
 	return class;
+}
+
+// Reads the patterns from a file and stores the data & class.
+void readFile( char *filename, double data[SIZE][LENGTH], char class[SIZE][NAME] ) {
+
+    FILE *fp;
+    fp = fopen(filename, "r");
+    if (!fp) {
+		puts("Error, file not found");
+        exit(1);
+	}
+	for ( size_t i = 0; i < SIZE; i++ ) {
+		fscanf(fp, "%lf,%lf,%lf,%lf,%s", &data[i][0], &data[i][1], &data[i][2], &data[i][3], &class[i][0]);
+	}
+    fclose(fp);
+
+    return ;
+}
+
+// Prints an array[N][M]
+void printArray( double array[SIZE][LENGTH] ) {
+
+    for ( size_t i = 0; i < SIZE; i++ ) {
+        for ( size_t j = 0; j < LENGTH; j++ )
+            printf("%lf ", array[i][j]);
+        putchar('\n');
+    }
+    putchar('\n');
+
+    return ;
+}
+
+// Prints the name of classes (class[SIZE][NAME])
+void printClass( char array[SIZE][NAME] ) {
+
+    for ( size_t i = 0; i < SIZE; i++ ) {
+    	printf("%s\n", array[i]);
+    }
+    putchar('\n');
+
+    return ;
+}
+
+void printMeans( double array[CENTERS][LENGTH] ) {
+
+	for ( size_t i = 0; i < CENTERS; i++ ) {
+		for ( size_t j = 0; j < LENGTH; j++ )
+			printf("%lf ", array[i][j]);
+		putchar('\n');
+	}
+	putchar('\n');
+
+	return ;
+}
+
+// Naive way function needs rewritting.
+int initialCenters( double means[CENTERS][LENGTH], char class[SIZE][NAME], double data[SIZE][LENGTH] ) {
+
+	int index[3] = {0, 55, 140};
+
+	for ( size_t i = 0; i < CENTERS; i++ ) {
+		for ( size_t j = 0; j < LENGTH; j++ ) {
+			means[i][j] = data[index[i]][j];
+		}
+	}
+
+	return 0;
+}
+
+// naive way
+void error( double means[CENTERS][LENGTH], double data[SIZE][LENGTH] ) {
+
+	double distances[SIZE][CENTERS];
+
+	for ( int i = 0; i < SIZE; i++ ) {
+		for ( int j = 0; j < CENTERS; j++ ) {
+			distances[i][j] = distEucl( data[i], means[j] );
+		}
+	}
+
+	int class;
+	int errorC = 0;
+	for ( int i = 0; i < SIZE; i++ ) {
+		class = argMin(distances[i]) + 1;
+		// class 1 is from 0 to 49
+		if ( (i < 50) && (class != 1) )
+			errorC += 1;
+		// class 2 is from 50 to 99
+		if ( (i > 49) && (i < 100) && (class != 2) )
+			errorC += 1;
+		if ( (i > 100) && (class != 3) )
+			errorC += 1;
+	}
+	printf("Wrong classifications: %d\nTotal patterns: 149 (%f)", errorC, errorC/(float) SIZE);
+
+	return ;
 }
--- KMeans/src/KMeans.c
+++ KMeans/src/KMeans.c
@@ -22,10 +22,10 @@ int main( void ) {
 	readFile( "iris.data", patterns, class );
 	initialCenters( means, class, patterns);
 	printMeans(means);
+	printf("\n");
 	KMeans(patterns, means, SIZE);
 	printMeans(means);
 	error(means, patterns);
-	//
 	double pattern[LENGTH];
 	pattern[0] = 5.1;
 	pattern[1] = 3.5;
@@ -37,105 +37,3 @@ int main( void ) {
 	return EXIT_SUCCESS;
 }
 
-// Reads the patterns from a file and stores the data & class.
-void readFile( char *filename, double data[SIZE][LENGTH], char class[SIZE][NAME] ) {
-
-    FILE *fp;
-    fp = fopen(filename, "r");
-    if (fp) {
-        for ( size_t i = 0; i < SIZE; i++ ) {
-            for ( size_t j = 0; j < LENGTH; j++ )
-                fscanf(fp,"%lf,", &data[i][j]);
-            fscanf(fp,"%s", &class[i][0]);
-        }
-    }
-    else {
-        puts("Error, file not found");
-        exit(1);
-    }
-    fclose(fp);
-
-    return ;
-}
-
-// Prints an array[N][M]
-void printArray( double array[SIZE][LENGTH] ) {
-
-    for ( size_t i = 0; i < SIZE; i++ ) {
-        for ( size_t j = 0; j < LENGTH; j++ )
-            printf("%lf ", array[i][j]);
-        putchar('\n');
-    }
-    putchar('\n');
-
-    return ;
-}
-
-// Prints the name of classes (class[SIZE][NAME])
-void printClass( char array[SIZE][NAME] ) {
-
-    for ( size_t i = 0; i < SIZE; i++ ) {
-    	printf("%s\n", array[i]);
-    }
-    putchar('\n');
-
-    return ;
-}
-
-// Naive way function needs rewritting.
-int initialCenters( double means[CENTERS][LENGTH], char class[SIZE][NAME], double data[SIZE][LENGTH] ) {
-
-	int index[3];
-	index[0] = 0;
-	index[1] = 55;
-	index[2] = 140;
-
-	for ( size_t i = 0; i < CENTERS; i++ ) {
-		for ( size_t j = 0; j < LENGTH; j++ ) {
-			means[i][j] = data[index[i]][j];
-		}
-	}
-
-	return 0;
-}
-
-void printMeans( double array[CENTERS][LENGTH] ) {
-
-	for ( size_t i = 0; i < CENTERS; i++ ) {
-		for ( size_t j = 0; j < LENGTH; j++ )
-			printf("%lf ", array[i][j]);
-		putchar('\n');
-	}
-	putchar('\n');
-
-	return ;
-}
-
-// naive way
-void error( double means[CENTERS][LENGTH], double data[SIZE][LENGTH] ) {
-
-	double distances[SIZE][CENTERS];
-
-	for ( int i = 0; i < SIZE; i++ ) {
-		for ( int j = 0; j < CENTERS; j++ ) {
-			distances[i][j] = distEucl( data[i], means[j] );
-		}
-	}
-
-	int class;
-	int errorC = 0;
-	for ( int i = 0; i < SIZE; i++ ) {
-		class = argMin(distances[i]) + 1;
-		// class 1 is from 0 to 49
-		if ( (i < 50) && (class != 1) )
-			errorC += 1;
-		// class 2 is from 50 to 99
-		else if ( (i > 49) && (i < 100) && (class != 2) )
-			errorC += 1;
-		else if ( (i > 100) && (class != 3) )
-			errorC += 1;
-	}
-	printf("Wrong classifications: %d\nTotal patterns: 149 (%f)", errorC, errorC/(float) SIZE);
-
-	return ;
-}
